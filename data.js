400b50429cd03b4826643000a4a1f0ff656130b8
PatoAcu <pato36423@gmail.com>
2023-08-31 11:56:57 -0300

    segundo commit

--- data.js
+++ data.js
@@ -4,13 +4,14 @@ const productsData = [
     name: "noticia-uno",
     user: "thetroncous",
     category: "deportes",
-    cardImg: "./assets/img/products/goldenmessi.png",
+    cardImg:
+      "imagenes noticias/boca_vs_racing_crop1621285881099.png_1159711837.png",
   },
   {
     id: 2,
     name: "noticia-dos",
     user: "thetroncous",
     category: "futbol",
-    cardImg: "./assets/img/products/goldenmessi.png",
+    cardImg: "",
   },
 ];
